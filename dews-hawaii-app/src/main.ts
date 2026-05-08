d47f71e05572f7818370e158b672bb4ea706b87f
cherryleh <cherryle@hawaii.edu>
2026-05-08 09:44:29 -1000

    Update main.ts

--- dews-hawaii-app/src/main.ts
+++ dews-hawaii-app/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+
+console.log(`Environment: ${environment.production ? 'environment.prod.ts (production)' : 'environment.ts (development)'}`, environment);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
