3f80edb7d67bee80afd8bab39ecab6f901c7ca3f
cherryleh <cherryle@hawaii.edu>
2026-07-01 11:18:51 -1000

    Maintenance flag

--- dews-hawaii-app/src/app/app.component.ts
+++ dews-hawaii-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { FooterComponent } from './footer/footer.component';
@@ -6,11 +7,14 @@ import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  // Toggle this to show/hide the site-wide maintenance banner.
+  maintenanceMode = true;
+
   constructor(private router: Router) {}
 
   ngOnInit() {
